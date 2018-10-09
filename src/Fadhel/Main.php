32cc6c4278409b875045960aa499ff2c9046c3fb
FadhelFS <43891391+FadhelFS@users.noreply.github.com>
2018-10-09 21:10:07 +0300

    Update Main.php

--- src/Fadhel/Main.php
+++ src/Fadhel/Main.php
@@ -1,1 +1,63 @@
+<?php
 
+namespace Fadhel;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);    
+        $this->getLogger()->info(TextFormat::GREEN . "Enabled!");
+    }
+
+    public function onDisable() {
+$this->getServer()->getPluginManager()->registerEvents($this,$this);
+        $this->getLogger()->info(TextFormat::RED . "Disabled!");
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+        switch($cmd->getName()){                    
+            case "transfer":
+                if ($sender instanceof Player){
+                     $this->openMyForm($sender);
+                }else{     
+                     $sender->sendMesseage(TextFormat::RED . "Run the command in-game");
+                     return true;
+                }     
+            break;         
+            
+         }  
+        return true;                         
+    }
+   
+    public function openMyForm($player){ 
+        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        $form = $api->createSimpleForm(function (Player $player, int $data = null){
+            $result = $data;
+            if($result === null){
+                return true;
+            }             
+            switch($result){
+                case 0:
+                    $player->transfer("play.hyperlandsmc.net");
+                break;
+            }
+            
+            
+            });
+            $form->setTitle("§lTransferUI");
+            $form->setContent("Select server to transfer!");
+            $form->addButton("HyperLands");
+            $form->sendToPlayer($player);                  
+            return $form;                                            
+    }
+                                                                                                                                                                                                                                                                                          
+}
